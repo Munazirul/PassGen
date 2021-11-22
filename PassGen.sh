381bbf2204aa4000ffb0a8b9c84f0b2691951723
Munazirul <munazir7862@gmail.com>
2021-11-22 22:49:01 +0530

    First commit

---
+++ PassGen.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+echo "####################################################################"
+echo "#    __________                        ________                    #"
+echo "#    \______   \_____    ______ ______/  _____/  ____   ____       #"
+echo "#     |     ___/\__  \  /  ___//  ___/   \  ____/ __ \ /    \      #"
+echo "#     |    |     / __ \_\___ \ \___ \\     \_\  \  ___/|   |  \     #"
+echo "#     |____|    (____  /____  >____  >\______  /\___  >___|  /     #"
+echo "#                    \/     \/     \/        \/     \/     \/      #"
+echo "#     Developed by Munazir                                    v1.1 #"                                  
+echo "#     github: github.com/Munazirul/PassGen                         #"
+echo "#                                                                  #"
+echo "####################################################################"
+echo ""
+echo ""
+echo -e "Specify the name of output file (for example, if you want to generate the password for discord, enter 'discord'):\c"
+read output_file
+echo -e "Enter the password length:\c"
+read pass_length
+
+#generate password
+function generate(){
+    for P in $(seq 1);
+do
+    openssl rand -base64 48 | cut -c3-$pass_length
+    done > $output_file.txt
+    echo "Generating $pass_length digit password"
+    sleep 2
+    echo "Password is generated and saved in $output_file.txt"
+}
+generate
+
+#copy the generated password to clipboard
+function clipboard(){
+
+    cat $output_file.txt | xclip -sel clip
+}
+
+#write ssmtp file
+function ssmtp_write(){
+    echo "UseSTARTTLS=YES
+UserTLS=YES
+hostname=localhost
+root=postmaster
+mailhub=smtp.gmail.com:587
+AuthUser=$mail
+AuthPass=$password
+TLS_CA_FILE=/etc/ssl/certs/ca-certificates.crt
+FromLineOverride=YES" > /etc/ssmtp/ssmtp.conf
+}
+#configure ssmtp
+function ssmtp_conf(){
+    if [[ `command -v ssmtp` || `command -v mpack` ]];
+        then 
+            ssmtp_write
+            else 
+            echo "Required packages are not installed. Installing it for you."
+            sleep 1
+            sudo bash -c 'apt-get -y install ssmtp -y && apt-get install mpack -y >/dev/null 2>&1 & disown'
+            echo "Required packages are Installed"
+            sleep 2
+            ssmtp_write
+            fi
+}
+
+#output
+function pass_gen(){
+    if [[ $ynmail == 'y' || $ynmail == 'Y' ]]
+        then 
+        echo "Before using this feature, Make sure that you have 
+enabled less secure app access under 'security' in your google account!"
+        echo -e "Enter your gmail address:\c"
+        read mail
+        echo -e "Enter your Password:\c"
+        read -s password
+        ssmtp_conf
+        sleep 1
+        mpack -s "Password generated using PassGen for $output_file.txt" $output_file.txt $mail
+        echo "The file containing password has been sent to your gmail"
+        # elif [[ $ynmail == 'n' || $ynmail == 'N' ]]
+        # then
+        exit 0
+        else
+        printf "\nYour password has been saved to $output_file.txt\n"
+        sleep 1
+            echo "Thank you for using PassGen, See you next time...."
+            exit 0
+        fi
+}
+echo -e "Do you want to copy the generated password to clipboard (Y/N) ?: "
+read yn
+if [[ $yn == 'y' || $yn == 'Y' ]]
+       then 
+       if [[ `command -v xclip` && `command -v clip` ]];
+       then
+       clipboard
+        else
+            sudo bash -c 'apt-get -y install xclip -y && apt-get install clip -y >/dev/null 2>&1 & disown'
+            clipboard
+            fi
+    echo "Your password has been copied to clipboard"
+    exit 0
+    elif [[ $yn == 'n' || $yn == 'N' ]]
+       then
+    echo -e "Do you want to send the password to your gmail (Y/N) ?: "
+    read ynmail
+    pass_gen
+    else 
+    echo "Invalid Input!!"
+    exit 0 
+    fi 
